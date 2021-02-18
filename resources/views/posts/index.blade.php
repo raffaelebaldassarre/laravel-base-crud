56a033d150212167d7d67b9d57955e9ffba727f8
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-02-18 20:36:01 +0100

    Add page Create

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,14 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Blog</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
     
     <h1>Laravel Blog</h1>
-    @foreach ($posts as $post)
-        <h2>{{$post->title}}</h2>
-        <p>{{$post->body}}</p>
-    @endforeach
+    <table class="table">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>title</th>
+                <th>body</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($posts as $value)
+            <tr>
+                <td>{{$value->id}}</td>
+                <td>{{$value->title}}</td>
+                <td>{{$value->body}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <a href="{{route('posts.create')}}">Crea un nuovo Post</a>
 
 </body>
 </html>
