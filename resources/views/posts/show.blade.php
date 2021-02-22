aaf03eaa28dd9908be14d2efe7d87f67b4832c67
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-02-22 10:08:21 +0100

    Debug

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,15 +6,12 @@
     <button type="button" class="btn btn-light btn-ms"> 
       <a href="{{ route('posts.index') }}">Ritorna all'elenco dei Post</a> 
     </button>
-        <a class="card-link" href="/">
-          <article class="blog-card">
+          <div class="blog-card">
             <div class="article-details">
               <h2 class="post-title"><?php echo "{$post->title}";?></h2>
               <p class="post-description"><?php echo " {$post->body}" ;?></p>
             </div>
-            <!-- /.article-details -->
-          </article>
-        </a>
+          </div>
   </div>
 
 @endsection
